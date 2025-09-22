1323b5c6fd4349efccf7cc010051ebc8fd9e5958
ST10249863-TiffanyMather <ST10249863@vcconnect.edu.za>
2025-09-22 23:07:35 +0200

    Backend: Updated Login and Register

--- src/index.js
+++ src/index.js
@@ -56,5 +56,8 @@ app.use((err, req, res, next) => {
 // -----------------------------
 // Start Server
 // -----------------------------
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => console.log('🚀 Server listening on', PORT))
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
